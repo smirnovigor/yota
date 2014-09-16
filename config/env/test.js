9398bf0ed2d8bfa359b84434298af243fd38b5b6
igor <1i9g8o3r>
2014-09-16 14:46:04 +0300

    Add meanjs seed

---
+++ config/env/test.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+	db: 'yota-test:yota-test@ds035740.mongolab.com:35740/yota',
+	port: 3001,
+	app: {
+		title: 'YOTA - Test Environment'
+	},
+	facebook: {
+		clientID: process.env.FACEBOOK_ID || 'APP_ID',
+		clientSecret: process.env.FACEBOOK_SECRET || 'APP_SECRET',
+		callbackURL: 'http://localhost:3000/auth/facebook/callback'
+	},
+	twitter: {
+		clientID: process.env.TWITTER_KEY || 'CONSUMER_KEY',
+		clientSecret: process.env.TWITTER_SECRET || 'CONSUMER_SECRET',
+		callbackURL: 'http://localhost:3000/auth/twitter/callback'
+	},
+	google: {
+		clientID: process.env.GOOGLE_ID || 'APP_ID',
+		clientSecret: process.env.GOOGLE_SECRET || 'APP_SECRET',
+		callbackURL: 'http://localhost:3000/auth/google/callback'
+	},
+	linkedin: {
+		clientID: process.env.LINKEDIN_ID || 'APP_ID',
+		clientSecret: process.env.LINKEDIN_SECRET || 'APP_SECRET',
+		callbackURL: 'http://localhost:3000/auth/linkedin/callback'
+	}
+};
